deca7cc4bb00b2339437c82c99e6a1199083ed2d
a.naoki.okamoto <a.naoki.okamoto@brainpad.co.jp>
2021-08-20 13:44:16 +0900

    remove unused import

--- cliboa/util/csv.py
+++ cliboa/util/csv.py
@@ -11,10 +11,7 @@
 # The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
 #
-import codecs
 import csv
-import os
-import shutil
 from cliboa.util.exception import CliboaException
 
 
