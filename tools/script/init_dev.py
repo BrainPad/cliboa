7e441eb509d76640b527fdad449623ae2ce90014
cocoa-maemae <breezin_ahimusa@yahoo.co.jp>
2022-08-26 03:18:40 +0900

    refactor pipenv to poetry

--- tools/script/init_dev.py
+++ tools/script/init_dev.py
@@ -7,12 +7,23 @@ from shutil import copyfile
 """
 Check current python version and prepare appropriate Pipfile
 """
-py_major_ver = "{}.{}".format(sys.version_info.major, sys.version_info.minor)
-py_major_ver_and_pipfile = {
+py_major_version = "{}.{}".format(sys.version_info.major, sys.version_info.minor)
+py_major_version_and_pipfile = {
     "3.6": "Pipfile.above36",
     "3.7": "Pipfile.above37",
     "3.8": "Pipfile.above38",
     "3.9": "Pipfile.above39",
 }
-pipfile_path = os.path.join("cliboa/template", py_major_ver_and_pipfile[py_major_ver])
+py_major_version_and_pyproject_toml = {
+    "3.6": "pyproject.above36.toml",
+    "3.7": "pyproject.above37.toml",
+    "3.8": "pyproject.above38.toml",
+    "3.9": "pyproject.above39.toml",
+}
+pipfile_path = os.path.join("cliboa/template", py_major_version_and_pipfile[py_major_version])
 copyfile(pipfile_path, os.path.join("./", "Pipfile"))
+
+pyproject_toml_path = os.path.join(
+    "cliboa/template", py_major_version_and_pyproject_toml[py_major_version]
+)
+copyfile(pyproject_toml_path, os.path.join("./", "pyproject.toml"))
