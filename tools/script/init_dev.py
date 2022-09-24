45d45966a73af0f19844326f55a4740d34b26c35
cocoa-maemae <breezin_ahimusa@yahoo.co.jp>
2022-09-24 17:51:15 +0900

    revert

--- tools/script/init_dev.py
+++ tools/script/init_dev.py
@@ -13,14 +13,12 @@ py_major_version_and_pipfile = {
     "3.7": "Pipfile.above37",
     "3.8": "Pipfile.above38",
     "3.9": "Pipfile.above39",
-    "3.10": "Pipfile.above310",
 }
 py_major_version_and_pyproject_toml = {
     "3.6": "pyproject.above36.toml",
     "3.7": "pyproject.above37.toml",
     "3.8": "pyproject.above38.toml",
     "3.9": "pyproject.above39.toml",
-    "3.10": "pyproject.above310.toml",
 }
 pipfile_path = os.path.join("cliboa/template", py_major_version_and_pipfile[py_major_version])
 copyfile(pipfile_path, os.path.join("./", "Pipfile"))
