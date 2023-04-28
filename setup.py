c32b57d6faaeb0132a5449e5936c7f2a20f3f2a0
Maeda Tomoki <breezin_ahimusa@yahoo.co.jp>
2023-04-28 18:15:10 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ def read(filename):
 
 setup(
     name="cliboa",
-    version="2.3.0b0",
+    version="2.3.1b0",
     description="application framework for ETL(ELT) processing",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
